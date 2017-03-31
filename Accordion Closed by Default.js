0e928d718329efbddee0c01c157ed92b69d5e90f
Dollhouse Digital <dollhousedigital@gmail.com>
2017-03-31 21:32:05 +0100

    Create Accordion Closed by Default.js

---
+++ Accordion Closed by Default.js
@@ -0,0 +1,4 @@
+jQuery(document).ready(function ()
+{
+  setTimeout(function () { jQuery('.elementor-widget-accordion .elementor-accordion-item:first-child .elementor-accordion-title').click(); }, 100);
+});
